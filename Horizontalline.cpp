a6d7afdaf46f3d710eca943a57e81ccb4b2bc9e4
Priya Anand <priiyaaa1102@gmail.com>
2025-08-06 18:50:27 +0530

    Update Horizontalline.cpp

--- Horizontalline.cpp
+++ Horizontalline.cpp
@@ -7,6 +7,14 @@ int main()
     int n;
     cout<<"Enter the length of horizontal line : ";
     cin>>n; //Takes input from the user.
+
+    //For invalid input cases
+    if (n<=0)
+{
+    cout<<"Enter a positive number!"<<endl;
+    return 1; //Exit if input is invalid ( 1 and anyother non-zero number is used for invalid cases i.e unsuccessfull cases )
+}
+
     for (int i=0 ; i<n; i++) //Loop runs n times
     {
         cout<<"* "; //Print * n times with a space
