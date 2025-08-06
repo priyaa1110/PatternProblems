5ffd33c195aae4f1ceae693c53ac8a4d2ebf4591
Priya Anand <priiyaaa1102@gmail.com>
2025-08-06 12:11:49 +0000

    Added the comments

--- Centeringhorizontalline.cpp
+++ Centeringhorizontalline.cpp
@@ -1,3 +1,6 @@
+//##Centering a horizontal line of any length at the center of a console using * symbol with spaces##
+
+
 #include<iostream>
 using namespace std;
 int main()
@@ -6,26 +9,37 @@ int main()
     cout<<"Enter the length of your horizontal line : ";
     cin>>n;
 
+    // Assuming standard console size (can vary on different systems)
     int console_width = 80;
     int console_height = 24;
      
-  
+  // Calculate vertical space to center the line vertically
     int vertical_spaces = (console_height - 1)/2;
+
+     // Calculate horizontal space to center the line horizontally
     int horizontal_spaces = (console_width - n)/2;
 
+
+    // Print blank lines to move the cursor vertically to the center
     for (int i =0; i<vertical_spaces; i++)
     {
         cout<<endl;
     }
+
+     // Print spaces to move the cursor horizontally to the center
     for (int i = 0; i<horizontal_spaces; i++)
     {
         cout<<" ";
     }
+    // Print the horizontal line of stars
     for (int i=0; i<n; i++)
     {
         cout<<"*";
     }
+    
+    // Move to the next line after printing
     cout<<endl;
+
     return 0;
 
 }
