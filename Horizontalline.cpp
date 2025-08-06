b86200e0da2d70e0d45c6cf2bd3ca3327dbec2ee
Priya Anand <priiyaaa1102@gmail.com>
2025-08-06 09:41:44 +0000

    Making a horizontal line with spaces using * symbol

---
+++ Horizontalline.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the length of horizontal line : ";
+    cin>>n;
+    for (int i=0 ; i<n; i++)
+    {
+        cout<<"* ";
+
+    }
+    cout<<endl;
+    return 0;
+}
