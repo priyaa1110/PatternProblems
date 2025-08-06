205d3e0f113e20a41f05b34d33e8ce79245a92de
Priya Anand <priiyaaa1102@gmail.com>
2025-08-06 15:30:18 +0000

    Renamed for clarity

---
+++ Hollow_horizontal_line.cpp
@@ -0,0 +1,41 @@
+//Code for printing a hollow horizontal line of any length using * symbol
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the length of your horizontal line : ";
+    cin >> n; // User enters the input
+
+    // For Invalid input cases
+    if (n <= 0)
+    {
+        cout << "Please enter a positive number!" << endl; 
+        return 1; //Exit if input is invalid ( 1 and anyother non-zero number is used for invalid cases i.e unsuccessfull cases )
+    }
+
+    for (int i = 0; i < n; i++) // Loop runs for  n times
+    {
+        if (i == 0 || i == n - 1) // Checks condition
+        {
+            cout << "*"; // Prints * for the first and last position
+        }
+        else
+        {
+            cout << " "; // Prints spaces for the rest of the position
+        }
+    }
+    
+    // Moves the cursor to the next line
+    cout << endl;
+
+    return 0;
+}
+
+/*
+OUTPUT
+Enter the length of your horizontal line : 7
+*     *
+*/
