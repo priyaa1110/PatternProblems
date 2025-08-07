8a29dd6b79a9abae7611a493ffd40d126fe73c63
Priya Anand <priiyaaa1102@gmail.com>
2025-08-07 03:48:16 +0000

    Updated

---
+++ All_star_patterns/Centering_cross_shape.cpp
@@ -0,0 +1,75 @@
+//Code for centering cross shape of any length using * symbol 
+
+#include<iostream>
+using namespace std;
+
+
+// Function to print a centered cross shape
+void print(int  n, int console_width, int console_height)
+{
+    int vertical_spaces =(console_height-n)/2;
+    int horizontal_spaces = (console_width-1)/2;
+
+    for (int i=0; i<vertical_spaces; i++)
+    {
+        cout<<endl;
+    }
+
+
+for (int i =0; i<n; i++)   // Loop through each row
+{ 
+    for(int j=0; j<horizontal_spaces; j++)
+{
+    cout<<" ";
+}
+    for (int j=0; j<n; j++)   // Loop through each column
+    {
+
+        // Print '*' if it's on either the left or right diagonal
+        if ((i==j) || (i+j == n-1))  // Left diagonal: i==j, Right diagonal: i+j==n-1
+        {
+            cout<<"*";  // Print star at diagonal positions
+           
+        }
+        else{
+            cout<<" ";  // Otherwise, print a space
+        }
+
+    }
+    cout<<endl;  // Move to the next line after printing each row
+}
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the length : ";
+    cin>>n;  // Take user input for size of the pattern
+     
+
+    // Check if the input is valid (positive number)
+    if(n<=0)
+    {
+        cout<<"Invalid input case!"<<endl;
+
+        return 1;
+    }
+
+    print(n, 80, 24);  // Call the function 
+
+    return 0;
+}
+
+/*
+
+OUTPUT
+Enter the lemgth : 5
+
+                                       *   *
+                                        * * 
+                                         *  
+                                        * * 
+                                       *   *
+                 
+  
+ */
