2a8bce3ff7e5f14fe5a9263bab150dd08af14d3c
Priya Anand <priiyaaa1102@gmail.com>
2025-08-08 14:27:06 +0000

    updated

--- All_star_shapes/Lines_and_basic_shapes_pattern/Square_shape.cpp
+++ All_star_shapes/Lines_and_basic_shapes_pattern/Square_shape.cpp
@@ -1,11 +1,8 @@
 // Code for printing square shape of any size using * symbol
 
 #include <iostream>
-#include <sstream>
-#include <string>
 using namespace std;
 
-stringstream buffer; 
 
 void print(int n)
 {
@@ -17,11 +14,11 @@ void print(int n)
 
         {
             cout << "* ";
-             buffer << "* "; 
+             
         }
 
         cout << endl;
-        buffer << "\n";
+    
     }
 }
 
@@ -41,29 +38,6 @@ int main()
     print(n);
 
     return 0;
-}
-
-// WebAssembly-compatible function
-extern "C"
-{
-    const char* run(int n)
-    {
-        static string result;      // ✅ Declare 'result' as static string
-        buffer.str("");            // Clear buffer content
-        buffer.clear();            // Clear buffer state
-
-        if (n <= 0)
-        {
-            buffer << "Invalid input case!\n";
-        }
-        else
-        {
-            print(n);              // Print into buffer
-        }
-
-        result = buffer.str();     // Copy buffer to static string
-        return result.c_str();     // Return the char* safely
-    }
 }
 
 
